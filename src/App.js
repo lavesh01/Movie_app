5660de56444153d6ead30ce2cfef917f08ff7009
Lave <laveshbisht01@gmail.com>
2023-02-15 22:03:06 +0530

    Add Movie detail page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
 
 import Header from "./components/header/Header"
 import Home from "./pages/home/Home";
+import MovieDetail from './pages/movieDetail/MovieDetail';
 import MovieList from './components/movieList/MovieList';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
         <Header />
         <Routes>
           <Route index element={<Home />}></Route>
-          <Route path="movie/:id" element={<h1>Movie id page</h1>}></Route>
+          <Route path="movie/:id" element={<MovieDetail />}></Route>
           <Route path="movies/:type" element={<MovieList />}></Route>
           <Route path="/*" element={<h1>Error page</h1>}></Route>
         </Routes>
