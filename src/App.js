9f3afd1bd50fe2c1705b29125ec7705cf32690fa
Lave <laveshbisht01@gmail.com>
2023-02-15 18:58:12 +0530

    Add Card feature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css'
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
 
 import Header from "./components/header/Header"
-import Home from './pages/Home';
+import Home from "./pages/home/Home";
+import MovieList from './components/movieList/MovieList';
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
         <Header />
         <Routes>
           <Route index element={<Home />}></Route>
-          <Route path="movie/:id" element={<h1>id page</h1>}></Route>
-          <Route path="movies/:type" element={<h1>Movies list page</h1>}></Route>
-          <Route path="/*" element={<h1>errow page</h1>}></Route>
+          <Route path="movie/:id" element={<h1>Movie id page</h1>}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1>Error page</h1>}></Route>
         </Routes>
       </Router>
     </div>
