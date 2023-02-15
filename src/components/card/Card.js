5d53985644ad3e9b454f3b2e4d4a7f2ebb50f4dd
Lave <laveshbisht01@gmail.com>
2023-02-15 21:21:17 +0530

    Render Cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -12,7 +12,7 @@ const Card = ({movie}) => {
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(false);
-        },5500)
+        },1500)
     },[]);
 
   return (
@@ -25,27 +25,24 @@ const Card = ({movie}) => {
                 </SkeletonTheme>
             </div> 
             :
-            <Link to={`/movie/${movie.id}`} style={{textDecoration:"none",color:"white"}} >
+            <Link to={`/movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
                 <div className="cards">
-                    
-                    <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie && movie.poster_path}`} alt="" />
+                    <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}` } alt="" />
                     <div className="cards__overlay">
-                        <div className="card__title"> {movie ? movie.original_title : ""}</div>
+                        <div className="card__title">{movie?movie.original_title:""}</div>
                         <div className="card__runtime">
-                            {movie ? movie.release_date : ""}
-                            <span className="card__rating">
-                            {movie ? movie.vote_average : ""}
-                            <i className="fas fa-star" />
-                            </span>
+                            {movie?movie.release_date:""}
+                            <span className="card__rating">{movie?movie.vote_average:""}<i className="fas fa-star" /></span>
                         </div>
-                        
-                        <div className="card__description"> {movie ? movie.original_overview.slice(0,118)+"..." : ""}</div>
+                        <div className="card__description">{movie ? movie.overview.slice(0,118)+"..." : ""}</div>
                     </div>
                 </div>
-            </Link>
+            </Link> 
+
         }
     </>
   )
 }
 
 export default Card
+
